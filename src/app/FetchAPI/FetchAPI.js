8e6d63b2f6ea57df91b25b23d49707de6174fb08
artikosamodra <artikosamudra@gmail.com>
2024-01-08 17:30:53 +0700

    Setting UI & UX TopList API Anime

---
+++ src/app/FetchAPI/FetchAPI.js
@@ -0,0 +1,19 @@
+//FetchAPI.js
+const TopFetch = async () => {
+  // Top Anime Fetch
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=12`
+  );
+  return response.json();
+};
+
+const RecomFetch = async () => {
+  // Recommendation Anime Fetch
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/recommendations/anime`
+  );
+  return response.json();
+};
+
+const AnimeFetch = { TopFetch, RecomFetch };
+export default AnimeFetch;
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,47 @@
+//page.js
 import Image from "next/image";
+import Link from "next/link";
+import AnimeList from "./AnimeList/AnimeList";
+import AnimeFetch from "./FetchAPI/FetchAPI";
 
-export default function Home() {
+const Home = async () => {
+  const topData = await AnimeFetch.TopFetch();
+  const recomData = await AnimeFetch.RecomFetch();
+
+  console.log(topData, recomData);
+
   return (
-    <main>
+    <main className="container-fluid mt-20 md:mx-10 mx-0 p-4">
+      {/* TOP LIST */}
+      <div className="flex justify-between pb-2">
+        <h1 className="font-bold text-lg">Top Anime List</h1>
+        <Link href="" className="font-bold underline text-blue-400">
+          Lihat Semua
+        </Link>
+      </div>
+
+      <div className="">
+        <div className="grid lg:grid-cols-6 md:grid-cols-4 grid-cols-2 gap-2">
+          {topData.data.map((data) => {
+            return (
+              <div key={data.mal_id} className="mb-5 title">
+                <AnimeList.TopList
+                  title={data.title}
+                  images={data.images.webp.image_url}
+                  id={data.mal_id}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      {/* RECOMMENDATIONS LIST */}
       <div>
-        <h1>INI HALAMAN AWAL WEB</h1>
+        <h1>Recommendation Anime List</h1>
       </div>
     </main>
   );
-}
+};
+
+export default Home;
