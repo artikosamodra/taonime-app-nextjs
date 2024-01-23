947c485b38f920fbcbbd041acc7652501b8bffbc
artikosamodra <artikosamudra@gmail.com>
2024-01-23 11:22:43 +0700

    add firstpage and lastpage button - pagination

--- src/app/component/Utilities/Pagination.jsx
+++ src/app/component/Utilities/Pagination.jsx
@@ -1,5 +1,11 @@
+import React, { useState } from "react";
+
 //setPage Recomendation if have very many child
 const Pagination = ({ pagePagination, lastPage, setPage }) => {
+  //Color Button
+  const [isNextButtonClicked, setNextButtonClicked] = useState(false);
+  const [isPrevButtonClicked, setPrevButtonClicked] = useState(false);
+
   //BackTop = if nextpage, view back on top page
   const scrollTop = () => {
     scrollTo({
@@ -10,12 +16,16 @@ const Pagination = ({ pagePagination, lastPage, setPage }) => {
 
   //handler Page Button
   const handleNextPage = () => {
+    setPrevButtonClicked(false);
+    setNextButtonClicked(true);
     setPage((prevState) => prevState + 1);
     scrollTop();
   };
   const handlePrevPage = () => {
     setPage((prevState) => {
       if (prevState > 1) {
+        setPrevButtonClicked(true);
+        setNextButtonClicked(false);
         scrollTop();
         return prevState - 1;
       } else {
@@ -24,29 +34,59 @@ const Pagination = ({ pagePagination, lastPage, setPage }) => {
     });
   };
 
+  const handleFirstPage = () => {
+    setPage((prevState) => (prevState = 1));
+    scrollTop();
+  };
+  const handleLastPage = () => {
+    setPage(lastPage);
+    scrollTop();
+  };
+
   return (
     <section>
-      <div className="flex justify-center items-center py-5 px-2 gap-5 font-bold">
-        {pagePagination <= 1 ? null : (
+      <div>
+        <div className="flex justify-center items-center py-5 px-2 gap-5 font-bold">
+          {pagePagination <= 1 ? null : (
+            <button
+              onClick={handlePrevPage}
+              className={`transition-all hover:text-indigo-500 ${
+                isPrevButtonClicked ? "text-indigo-500" : ""
+              }`}
+            >
+              Prev
+            </button>
+          )}
+          {/* this pagePagination is simple change value grom page in parent populer page */}
+          <p>
+            {pagePagination} of {lastPage}
+          </p>
+          {pagePagination >= lastPage ? null : (
+            <button
+              onClick={handleNextPage}
+              className={`transition-all hover:text-indigo-500 ${
+                isNextButtonClicked ? "text-indigo-500" : ""
+              }`}
+            >
+              Next
+            </button>
+          )}
+        </div>
+        <div className="flex justify-center items-center px-2 gap-5 font-bold">
           <button
-            onClick={handlePrevPage}
+            onClick={handleFirstPage}
             className="transition-all hover:text-indigo-500"
           >
-            Prev
+            Back to First Page
           </button>
-        )}
-        {/* this pagePagination is simple change value grom page in parent populer page */}
-        <p>
-          {pagePagination} of {lastPage}
-        </p>
-        {pagePagination >= lastPage ? null : (
+          <p>|</p>
           <button
-            onClick={handleNextPage}
+            onClick={handleLastPage}
             className="transition-all hover:text-indigo-500"
           >
-            Next
+            Go to Last Page
           </button>
-        )}
+        </div>
       </div>
     </section>
   );
